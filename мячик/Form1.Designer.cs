2155ee73c9bc8bece3bba80c9792f7788ed6709a
Alexander <gluhovaleksandr0129@gmail.com>
2022-09-26 23:02:43 +0500

    Добавьте файлы проекта.

---
+++ мячик/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿
+namespace мячик
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1022, 399);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Построить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1022, 15);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "100";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 19);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(756, 687);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1022, 54);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "100";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1022, 92);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 27);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "100";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1022, 131);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(125, 27);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "70";
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox5.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.textBox5.Location = new System.Drawing.Point(1022, 170);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(125, 27);
+            this.textBox5.TabIndex = 6;
+            this.textBox5.Text = "70";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(972, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "h";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(972, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "H";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(972, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "L";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(972, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "V";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(972, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Phi";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(972, 212);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Alpha";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(972, 251);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 20);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "m";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(1022, 248);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(125, 27);
+            this.textBox7.TabIndex = 15;
+            this.textBox7.Text = "50";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(1022, 286);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(125, 27);
+            this.textBox6.TabIndex = 19;
+            this.textBox6.Text = "10";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(1022, 209);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(125, 27);
+            this.textBox8.TabIndex = 20;
+            this.textBox8.Text = "1";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(972, 291);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(17, 20);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "T";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(972, 325);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 20);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "n";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(1022, 325);
+            this.textBox9.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(125, 27);
+            this.textBox9.TabIndex = 24;
+            this.textBox9.Text = "1000";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(1022, 484);
+            this.textBox10.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(125, 27);
+            this.textBox10.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1176, 895);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+    }
+}
+
---
+++ мячик/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace мячик
+{
+    public partial class Form1 : Form
+    {
+        string stfolder;
+        public Form1()
+        {
+            InitializeComponent();
+            stfolder = @"C:\D\unversity\Programming\расчет фортран графики";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //preparation
+            //IFormatProvider formatter = new NumberFormatInfo { NumberDecimalSeparator = "." };
+            int n, j;
+            double H, L;
+
+            //H = double.Parse(textBox2.Text, formatter);
+            Console.WriteLine(textBox2.Text);
+            //L = double.Parse(textBox3.Text, formatter);
+            H = double.Parse(textBox2.Text);
+            //L = double.Parse(textBox3.Text);
+            //n = int.Parse(textBox9.Text);
+            //H = Convert.ToDouble(textBox2.Text);
+            L = Convert.ToDouble(textBox3.Text);
+            n = Convert.ToInt32(textBox9.Text);
+            string st;
+            st = textBox1.Text + " , " + textBox4.Text + " , " + textBox5.Text + " , " + textBox8.Text + " , " + textBox7.Text + " , " + textBox6.Text + " , " + textBox9.Text;
+            StreamWriter info_for_ft;
+            info_for_ft = new StreamWriter(stfolder + "\\in.txt");
+            info_for_ft.Write(st);
+            info_for_ft.Write("\n");
+            info_for_ft.Write("hight , Velosity , phi , alpha , mass , Time , n");
+            info_for_ft.Close();
+
+            //System.Diagnostics.ProcessStartInfo ft = new System.Diagnostics.ProcessStartInfo(stfolder + "\\Pen.exe");
+            //ft.WorkingDirectory = stfolder;
+            using (Process pr = new Process())
+            {
+                pr.StartInfo = new ProcessStartInfo
+                {
+                    WorkingDirectory = stfolder,
+                    FileName = stfolder + "\\Pen.exe"
+                };
+                //pr.StartInfo = ft;
+                pr.Start();
+                pr.WaitForExit();
+            }
+
+            //System.Diagnostics.ProcessStartInfo ft = new System.Diagnostics.ProcessStartInfo(stfolder + "\\Pen.exe");
+            //ft.WorkingDirectory = stfolder;
+            //System.Diagnostics.Process.Start(ft);
+            //System.Threading.Thread.Sleep(5000);
+
+            // рисовалка
+            double mx, my;
+            Bitmap btmp;
+            btmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = btmp;
+            Graphics gr = Graphics.FromImage(pictureBox1.Image);
+            Pen pn = new Pen(Color.Green, 2);
+            SolidBrush pn1 = new SolidBrush(Color.Red);
+
+            double[] x, y;
+            x = new double[2 * n];
+            y = new double[2 * n];
+            
+            string st1;
+            StreamReader info_from_ft;
+            info_from_ft = new StreamReader(stfolder + "\\out.txt");
+            j = n;
+            for (int i = 0; !info_from_ft.EndOfStream; i++)
+            {
+                st1 = info_from_ft.ReadLine();
+                textBox10.Text = "ok1";
+                x[i] = double.Parse(st1.Substring((st1.IndexOf(";") + 3), 16));
+                y[i] = double.Parse(st1.Substring(st1.IndexOf(",") + 3));
+                //x[i] = double.Parse(st1.Substring((st1.IndexOf(";") + 3), 16), formatter);
+                //y[i] = double.Parse(st1.Substring(st1.IndexOf(",") + 3), formatter);
+                /*if (i % 2 == 0) 
+                {
+                    x[i] = double.Parse(st1.Trim());
+                    textBox10.Text = "ok2";
+                }
+                else 
+                {
+                    y[i] = Convert.ToDouble(st1.Trim());
+                    textBox10.Text = "ok3";
+                }*/
+                if (y[i] < 0.0)
+                {
+                    j = i;
+                    break;
+                }
+            }
+            this.Text = "ok4";
+            info_from_ft.Close();
+            info_from_ft.Dispose();
+
+            StreamWriter info;
+            info = new StreamWriter(stfolder + "\\in(a).txt");
+            for (int i = 0; i < j; i++)
+            {
+                st = x[i].ToString() + " " + y[i].ToString();
+                info.Write(st);
+                info.Write("\n");
+            }
+            info.Close();
+            info.Dispose();
+
+            mx = pictureBox1.Width / (x.Max() - x.Min());
+            my = pictureBox1.Height / (y.Max() - y.Min());
+
+            for (int i = 0; i < j - 1; i++) {
+                gr.DrawLine(pn, Convert.ToInt32((x[i] - x.Min()) * mx), Convert.ToInt32((y.Max() - y[i]) * my), Convert.ToInt32((x[i + 1] - x.Min()) * mx), Convert.ToInt32((y.Max() - y[i + 1]) * my));
+            }
+            //gr.FillEllipse(pn1, Convert.ToInt32((L - x.Min()) * mx), Convert.ToInt32((y.Max() - H) * my), 50, 50);
+            gr.FillEllipse(pn1, Convert.ToInt32(L / pictureBox1.Width), Convert.ToInt32((H - y.Min()) * my), 50, 50);
+        }
+
+    }
+}
